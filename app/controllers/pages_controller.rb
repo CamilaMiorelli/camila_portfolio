2b5ca8d5c65c259ced16dbd9639122f1ef8470b8
Camila Miorelli <camimiorelli@gmail.com>
2020-09-16 15:26:50 +0200

    landing page updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def cv
+  end
+
+  def work
+  end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'cv', to: 'pages#cv'
+  get 'work', to: 'pages#work'
+  get 'about', to: 'pages#about'
 end
